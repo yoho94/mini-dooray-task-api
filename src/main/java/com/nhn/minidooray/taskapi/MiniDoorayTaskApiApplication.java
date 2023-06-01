3545f9f42df0eb754ea0abe9b6791815a7d69014
kimhyunjun <rlaguswns94@naver.com>
2023-06-01 20:17:32 +0900

    Initial commit

---
+++ src/main/java/com/nhn/minidooray/taskapi/MiniDoorayTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.minidooray.taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDoorayTaskApiApplication.class, args);
+    }
+
+}
