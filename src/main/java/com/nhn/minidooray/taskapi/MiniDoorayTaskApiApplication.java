cee05816c88613ddede464f191decb7159b53f2e
dongyun <leedy6195@naver.com>
2023-06-02 20:09:17 +0900

    프로젝트 셋업

--- src/main/java/com/nhn/minidooray/taskapi/MiniDoorayTaskApiApplication.java
+++ src/main/java/com/nhn/minidooray/taskapi/MiniDoorayTaskApiApplication.java
@@ -2,8 +2,10 @@ package com.nhn.minidooray.taskapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.nhn.minidooray.taskapi.config")
 public class MiniDoorayTaskApiApplication {
 
     public static void main(String[] args) {
