f58a6d3b053150278d61614563c1bdc9df114169
CHOIGANGHYUEN <goaqjrj13@gmail.com>
2023-06-13 20:50:28 +0900

    테스크 서비스 임플

--- src/test/java/com/nhn/minidooray/taskapi/service/TaskServiceImplTest.java
+++ src/test/java/com/nhn/minidooray/taskapi/service/TaskServiceImplTest.java
@@ -3,6 +3,7 @@ package com.nhn.minidooray.taskapi.service;
 import com.nhn.minidooray.taskapi.domain.request.TaskCreateRequest;
 import com.nhn.minidooray.taskapi.domain.request.TaskUpdateRequest;
 import com.nhn.minidooray.taskapi.domain.response.ProjectAccountResponse;
+import com.nhn.minidooray.taskapi.domain.response.TasksResponse;
 import com.nhn.minidooray.taskapi.entity.MilestoneEntity;
 import com.nhn.minidooray.taskapi.entity.TaskEntity;
 import com.nhn.minidooray.taskapi.repository.AuthorityRepository;
@@ -11,6 +12,10 @@ import com.nhn.minidooray.taskapi.repository.ProjectAccountRepository;
 import com.nhn.minidooray.taskapi.repository.ProjectRepository;
 import com.nhn.minidooray.taskapi.repository.ProjectStateRepository;
 import com.nhn.minidooray.taskapi.repository.TaskRepository;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import net.bytebuddy.asm.Advice;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,6 +45,7 @@ import com.nhn.minidooray.taskapi.entity.ProjectStateEntity;
 import com.nhn.minidooray.taskapi.enumerate.AuthorityType;
 import com.nhn.minidooray.taskapi.enumerate.ProjectStateType;
 import com.nhn.minidooray.taskapi.exception.NotFoundException;
+import org.springframework.data.domain.Pageable;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
@@ -124,8 +130,9 @@ public class TaskServiceImplTest {
 
     @Test
     void updateTask_o() {
-        // Given
         Long taskId = 1L;
+        AtomicInteger idGenerator = new AtomicInteger(1); // 자동 증가를 위한 id 생성기
+
         TaskUpdateRequest taskUpdateRequest = new TaskUpdateRequest();
         taskUpdateRequest.setName("updated task");
         taskUpdateRequest.setProjectId(1L);
@@ -138,11 +145,13 @@ public class TaskServiceImplTest {
             .build();
 
         ProjectEntity projectEntity = ProjectEntity.builder()
+            .id(1L)
             .name("test project")
             .createAt(LocalDateTime.now())
             .build();
 
         MilestoneEntity milestoneEntity = MilestoneEntity.builder()
+            .id(1L)
             .build();
 
         ProjectAccountEntity.Pk projectAccountPk = ProjectAccountEntity.Pk.builder()
@@ -150,37 +159,109 @@ public class TaskServiceImplTest {
             .accountId("test writer")
             .build();
 
-        ProjectAccountEntity projectAccountEntity = ProjectAccountEntity.builder()
-            .pk(projectAccountPk)
-            .build();
-
         // When
         when(taskRepository.findById(taskId)).thenReturn(Optional.of(taskEntity));
         when(projectRepository.findById(taskUpdateRequest.getProjectId())).thenReturn(Optional.of(projectEntity));
         when(milestoneRepository.findById(taskUpdateRequest.getMilestoneId())).thenReturn(Optional.of(milestoneEntity));
-        // Given
-        when(projectAccountRepository.findByPk(any(ProjectAccountEntity.Pk.class))).thenAnswer((invocation) -> {
-            ProjectAccountEntity.Pk pk = invocation.getArgument(0);
-            if (pk.equals(projectAccountPk)) {
-                return Optional.of(new ProjectAccountResponse() {
-                    @Override
-                    public String getAuthorityEntityCode() {
-                        return "관리자";
-                    }
-                });
+
+        when(projectAccountRepository.findByPk(any(ProjectAccountEntity.Pk.class))).thenReturn(Optional.of(new ProjectAccountResponse() {
+            @Override
+            public String getAuthorityEntityCode() {
+                return "관리자";
             }
-            return Optional.empty();
+        }));
+
+        when(taskRepository.save(any(TaskEntity.class))).thenAnswer((invocation) -> {
+            TaskEntity entity = invocation.getArgument(0);
+            if (entity.getId() == null) {
+                entity.setId((long) idGenerator.getAndIncrement()); // id 설정
+            }
+            return entity;
         });
-        when(taskRepository.save(any(TaskEntity.class))).thenReturn(taskEntity);
 
         // Then
         Long updatedTaskId = taskService.updateTask(taskId, taskUpdateRequest);
         assertNotNull(updatedTaskId);
         assertEquals(taskId, updatedTaskId);
-        assertEquals(taskUpdateRequest.getName(), taskEntity.getTitle());
-        assertEquals(taskUpdateRequest.getProjectId(), taskEntity.getProjectEntity().getId());
-        assertEquals(taskUpdateRequest.getWriterId(), taskEntity.getWriterId());
-        assertEquals(taskUpdateRequest.getMilestoneId(), taskEntity.getMilestoneEntity().getId());
+        TaskEntity updatedTaskEntity = taskRepository.findById(updatedTaskId).orElseThrow();
+        assertEquals(taskUpdateRequest.getName(), updatedTaskEntity.getTitle());
+        assertEquals(taskUpdateRequest.getWriterId(), updatedTaskEntity.getWriterId());
+        assertEquals(taskUpdateRequest.getProjectId(), updatedTaskEntity.getProjectEntity().getId());
+        assertEquals(taskUpdateRequest.getMilestoneId(), updatedTaskEntity.getMilestoneEntity().getId());
+
+    }
+    @Test
+    void deleteTaskTest_notFound() {
+        // Given
+        Long taskId = 1L;
+
+        // When
+        when(taskRepository.findById(taskId)).thenReturn(Optional.empty());
+
+        // Then
+        assertThrows(NotFoundException.class, () -> taskService.deleteTask(taskId));
+    }
+    @Test
+    void getTasksTest() {
+        // Given
+        Long projectId = 1L;
+        Pageable pageable = PageRequest.of(0, 5); // 1st page with 5 items
+        List<TasksResponse> tasks = IntStream.range(0, 5)
+            .mapToObj(i -> {
+                TasksResponse tasksResponse = new TasksResponse(){
+                    @Override
+                    public Long getProjectEntityId() {
+                        return (long)i;
+                    }
+
+                    @Override
+                    public Long getId() {
+                        return (long)i;
+                    }
+
+                    @Override
+                    public String getTitle() {
+                        return "test title :"+i;
+                    }
+
+                    @Override
+                    public String getContent() {
+                        return "test content :"+i;
+                    }
+
+                    @Override
+                    public String getWriterId() {
+                        return "test writer :"+i;
+                    }
+
+                    @Override
+                    public LocalDateTime getCreateAt() {
+                        return LocalDateTime.now().minusDays( i );
+                    }
+
+                };
+                return tasksResponse;
+            }
+            )
+            .collect( Collectors.toList());
+
+        Page<TasksResponse> taskPage = new PageImpl<>(tasks, pageable, tasks.size());
+
+        when(taskRepository.findAllByProjectEntity_Id(anyLong(), any(Pageable.class))).thenReturn(taskPage);
+
+        // When
+        Page<TasksResponse> returnedPage = taskService.getTasks(projectId, pageable);
+
+        // Then
+        assertNotNull(returnedPage);
+        assertEquals(5, returnedPage.getContent().size());
+
+        IntStream.range(0, 5).forEach(i -> {
+            assertEquals(tasks.get(i).getId(), returnedPage.getContent().get(i).getId());
+            assertEquals(tasks.get(i).getTitle(), returnedPage.getContent().get(i).getTitle());
+        });
+
+        verify(taskRepository, times(1)).findAllByProjectEntity_Id(projectId, pageable);
     }
 
 }
