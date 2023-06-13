0047c891167171dd095ccfecf724a418f664a74a
dongyun <leedy6195@naver.com>
2023-06-13 17:42:23 +0900

    test: TagServiceImplTest

--- src/main/java/com/nhn/minidooray/taskapi/domain/request/TagCreateRequest.java
+++ src/main/java/com/nhn/minidooray/taskapi/domain/request/TagCreateRequest.java
@@ -1,8 +1,10 @@
 package com.nhn.minidooray.taskapi.domain.request;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class TagCreateRequest {
     private String name;
 }
--- src/main/java/com/nhn/minidooray/taskapi/domain/response/TagByProjectResponse.java
+++ src/main/java/com/nhn/minidooray/taskapi/domain/response/TagByProjectResponse.java
@@ -1,11 +1,14 @@
 package com.nhn.minidooray.taskapi.domain.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class TagByProjectResponse {
     Long projectId;
     Long tagId;
